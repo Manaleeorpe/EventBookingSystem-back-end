d1be06bd4d38260869dfe454f0653aebf9004550
Manaleeorpe <71582885+Manaleeorpe@users.noreply.github.com>
2025-09-29 23:34:03 +0530

    updated google routes

--- src/routes/google.routes.js
+++ src/routes/google.routes.js
@@ -2,11 +2,11 @@ const { Router } = require("express");
 const ctrl = require("../controllers/google.controller");
 
 const router = Router();
-router.get("/login",requireAuth,requireAuth, ctrl.googleLogin);
-router.get("/callback",requireAuth, ctrl.googleCallback);
-router.get("/logout",requireAuth, ctrl.googleLogout);
-router.get("/failure",requireAuth, ctrl.googleFailure);
-router.get("/me",requireAuth, ctrl.me);
+router.get("/login", ctrl.googleLogin);
+router.get("/callback", ctrl.googleCallback);
+router.get("/logout", ctrl.googleLogout);
+router.get("/failure", ctrl.googleFailure);
+router.get("/me", ctrl.me);
 
 
 module.exports = router;
